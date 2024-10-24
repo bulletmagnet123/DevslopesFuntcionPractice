ef8f0d12fbf73ca1de724648384c4df279c705dd
bulletmagnet <bulletmagnet.123@gmail.com>
2024-10-23 22:49:07 -0700

    e4 done only one left

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,9 +9,10 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
+// function splitFirstAndLastNames(str) {
+//   return str.split(' ');
+// }
+const splitFirstAndLastNames = (str) => {return str.split(' ');}
 
 
 /** =========================
@@ -20,9 +21,10 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+// const personAge = function(personObject) {
+//   return personObject.age;
+// 
+const personAge = (personObject) => {return personObject.age}
 
 
 /** =========================
@@ -32,10 +34,10 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
-
+// const isNameInArray = function(arr, name) {
+//   return arr.includes(name);
+// }
+const isNameInArray = (arr, name) => {return arr.includes(name)}
 
 /** =========================
  * The logTimer function takes a number as the arguments.
@@ -44,16 +46,27 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
-  let i = 0;
-  const timer = setInterval(function() {
-    if (i < max) {
-      console.log(++i);
-    } else {
-      clearInterval(timer);
-    }
-  }, 1000);
-}
+// const logSecondsUpToMax = function(max) {
+//   let i = 0;
+//   const timer = setInterval(function() {
+//     if (i < max) {
+//       console.log(++i);
+//     } else {
+//       clearInterval(timer);
+//     }
+//   }, 1000);
+// }
+
+const logSecondsUpToMax = (max) => {
+    let i = 0;
+    const timer = setInterval(() => {
+        if (i < max) {
+            console.log(++i);
+        } else {
+            clearInterval(timer);
+        }
+    }, 1000);
+};
 
 
 
